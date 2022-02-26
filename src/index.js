27f315202a43c14dd19f2d39651fcfb2e5996464
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2022-02-26 21:53:32 +0530

    added env port

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app)
 const io = socketio(server);
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 // public dir to serve html files
 const publicDirectoryPath = path.join(__dirname, '../public');
 
